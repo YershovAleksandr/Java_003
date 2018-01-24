b975e8d447e71770b6f5db5c6139539a5f2ff6a6
hp <namstudionsk@gmail.com>
2018-01-25 00:21:07 +0700

    Fix

--- src/com/nam/Main.java
+++ src/com/nam/Main.java
@@ -2,8 +2,21 @@ package com.nam;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws java.io.IOException{
 	// write your code here
         System.out.println("Hello World Motherfucka");
+
+        int nums[] = {4, 8, 15, 16, 32, 64, 128, 256, 516, 1024, 2048, 4096, 8105, 16325};
+
+        int sum = 0;
+
+        int k = 0;
+
+        for(int x: nums){
+            sum += x;
+            System.out.println(k++ + " x = " + x);
+        }
+
+        System.out.println("\nSum = " + sum);
     }
 }
